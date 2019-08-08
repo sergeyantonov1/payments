409683f15336da0d6375e54753ca33c45998f60c
Sergey Antonov <sergey.antonov@flatstack.com>
2019-08-08 12:16:16 +0300

    Add activeadmin (#2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  ActiveAdmin.routes(self)
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
